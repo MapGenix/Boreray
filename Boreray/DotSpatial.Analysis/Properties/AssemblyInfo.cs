9e4d0c370e0618b061c466a94badf0a7f5345a2f
Francois Mottard <francoismottard30@gmail.com>
2015-06-23 17:10:18 -0500

    Add DotSpatial.Analysis

---
+++ Boreray/DotSpatial.Analysis/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+// *******************************************************************************************************
+// File: C:\dev\DotSpatial\DotSpatial.Analysis\Properties\AssemblyInfo.cs
+// Description: Contains assembly information for this assembly 
+// Copyright & License: See www.DotSpatial.org.
+// *******************************************************************************************************
+// Contributor(s): Open source contributors may list themselves and their modifications here.
+// Contribution of code constitutes transferral of copyright to the DotSpatial Team.
+// *******************************************************************************************************
+
+ 
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Boreray.Analysis")]
+[assembly:
+    AssemblyDescription(
+        "Analysis and manipulation classes, such as clip raster with polygon, which are generally wrapped by a tool.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MapGenix")]
+[assembly: AssemblyProduct("Boreray.Analysis")]
+[assembly: AssemblyCopyright("MapGenix")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("f2aa48df-b877-40a9-853d-e0fd5b38f0c7")]
+[assembly: AssemblyVersion("0.0.0.1")]
