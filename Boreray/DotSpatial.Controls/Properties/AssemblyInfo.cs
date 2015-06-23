410dc37b68877225dd2aa971918c2b758445b9a9
Francois Mottard <francoismottard30@gmail.com>
2015-06-23 09:11:40 -0500

    Initial commit

---
+++ Boreray/DotSpatial.Controls/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+// *******************************************************************************************************
+// File: C:\dev\DotSpatial\DotSpatial.Controls\Properties\AssemblyInfo.cs
+// Description: Contains assembly information for this assembly 
+// Copyright & License: See www.DotSpatial.org.
+// *******************************************************************************************************
+// Contributor(s): Open source contributors may list themselves and their modifications here.
+// Contribution of code constitutes transferral of copyright to the DotSpatial Team.
+// *******************************************************************************************************
+
+ 
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Boreray.Controls")]
+[assembly: AssemblyDescription("Windows Forms controls, includes a map control and legend control.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MapGenix")]
+[assembly: AssemblyProduct("Boreray.Controls")]
+[assembly: AssemblyCopyright("MapGenix")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86a8a37c-da91-46b5-ac7e-f559d5763011")]
+
+[assembly: AssemblyVersion("0.0.0.2")]
